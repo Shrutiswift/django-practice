e3b3693679726bd2b5d3a7568a5bddb8ef7edab1
Shruti <shrutiswift@gmail.com>
2021-07-29 19:53:37 +0530

    Remove Task

---
+++ tasks/views.py
@@ -0,0 +1,27 @@
+from tasks.forms import TaskForm
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from .models import *
+from .forms import *
+  
+def index(request):
+    tasks = Task.objects.all()
+    
+    form = TaskForm()
+
+    if request.method =='POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks,'form': form}
+    return render(request, 'list.html', context)
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    return render(request, 'update_task.html')
+
